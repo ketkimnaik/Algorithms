f9e0c490bb98688949a759c71ac108dffa421312
Ketki <ketkimnaik@gmail.com>
2020-03-17 00:07:40 -0400

    kth Smallest elemnt in sorted matrix: 378

---
+++ 378_Kth _Smallest_Element_in_a_Sorted_Matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        
+        if(matrix.size()==0)
+           return 0;
+        priority_queue<int> pq;
+        
+        for(int i=0;i<matrix.size();++i)
+        {
+            for(int j=0;j<matrix[0].size();++j)
+            {
+                pq.push(matrix[i][j]);
+                
+            }
+        }
+        
+        while(pq.size() !=k)
+            pq.pop();
+        
+        return pq.top();
+    }
+};
