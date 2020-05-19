cefa01fe54f6dd5d900bee565b4a2ac138be30f5
Ketki <ketkimnaik@gmail.com>
2020-05-19 13:31:32 -0500

    Delete leaves with given values: 1325

---
+++ 1325_Delete_Leaves_With_a_Given_Value.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void DFS(TreeNode* &root,int target)
+    {
+        if(!root)
+            return;
+        DFS(root->left,target);
+        DFS(root->right,target);
+        
+        if(root->right==nullptr && root->left==nullptr && root->val==target)
+            root=nullptr;
+    }
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        
+        DFS(root,target);
+        return root;
+        
+    }
+};
