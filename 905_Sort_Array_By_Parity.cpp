ef6b7a4c96cdfc9c7d15408be282410a5740094e
Ketki <ketkimnaik@gmail.com>
2020-05-19 12:35:18 -0500

    Sort array by parity: 905

---
+++ 905_Sort_Array_By_Parity.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+       vector<int> v(A.size());
+        int l = 0;
+        int r = A.size() - 1;
+        int i = 0;
+        while( l <= r && i < A.size()){
+            if(A[i]%2 == 0)
+                v[l++] = A[i++];
+            else
+                v[r--] = A[i++];
+        }
+        return v;
+    }
+};
