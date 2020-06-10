36e50c0b6e1f11899de5c665327a88e0022bc3e2
Ketki <ketkimnaik@gmail.com>
2020-06-09 20:31:49 -0700

    Sort Array By Parity II: 922

---
+++ 922_Sort_Array_By_Parity_II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& A) {
+        vector<int> result(A.size());
+        
+        int even = 0, odd = 1;
+        for(int i = 0; i < A.size(); ++i) {
+            if(A[i] % 2 == 0) {
+                result[even] = A[i];
+                even += 2;
+            }
+            else {
+                result[odd] = A[i];
+                odd += 2;
+            }
+        }
+        
+        return result;
+    }
+};
